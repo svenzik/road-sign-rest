142dd62472cf81f9b0f51e41774e7950af11e344
S <svenzik@gmail.com>
2017-05-10 10:16:02 +0100

    rearranged test data

---
+++ test/road-sign-rest-client.sh
@@ -0,0 +1,15 @@
+
+HOST=http://localhost:9001
+METHOD=$1
+FILE=$2
+
+URL=${HOST}/${METHOD}
+DATE=`date +%Y%m%d_%H%M%S`
+JSON_FILE=${DATE}.json
+IMG_FILE=${DATE}.jpg
+
+echo Connecting to $URL
+curl ${URL} -H "Content-Type: application/json" -d @${FILE} > $JSON_FILE
+grep 'image_path' $JSON_FILE | sed -e 's/image_path/speed/' 
+grep '\"image\"' $JSON_FILE | sed -e 's/.*: *//;s/[",]//g' | base64 -d > $IMG_FILE
+

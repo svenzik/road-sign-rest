0599b7cb696d95061c879cb520e726f9651774a5
S <svenzik@gmail.com>
2017-05-23 08:34:42 +0100

    cleanded test data

--- test/road-sign-rest-client.sh
+++ test/road-sign-rest-client.sh
@@ -1,13 +1,19 @@
 
 HOST=http://localhost:9001
 METHOD=$1
-IMG_FILE=$2
-FILE=`./create-test-pacakge-from-image $IMG_FILE`
+IMG_SRC_FILE=$2
+FILE=`./create-test-pacakge-from-image $IMG_SRC_FILE`
 
 URL=${HOST}/${METHOD}
 DATE=`date +%Y%m%d_%H%M%S`
 JSON_FILE=${DATE}.json
 IMG_FILE=${DATE}.jpg
+
+echo Settings:
+echo IMG=$IMG_SRC_FILE
+echo REQUEST_JSON=$FILE
+echo RESULT_JSON=$JSON_FILE
+echo RESULT_IMG=$IMG_FILE
 
 echo Connecting to $URL
 curl ${URL} -H "Content-Type: application/json" -d @${FILE} > $JSON_FILE

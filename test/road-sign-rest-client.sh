aee2dfec9d906ab45c1084d9b30f4bf5b67ae6eb
S <svenzik@gmail.com>
2017-05-23 07:18:02 +0100

    update test client

--- test/road-sign-rest-client.sh
+++ test/road-sign-rest-client.sh
@@ -1,7 +1,8 @@
 
 HOST=http://localhost:9001
 METHOD=$1
-FILE=$2
+IMG_FILE=$2
+FILE=`./create-test-pacakge-from-image $IMG_FILE`
 
 URL=${HOST}/${METHOD}
 DATE=`date +%Y%m%d_%H%M%S`
@@ -10,6 +11,5 @@ IMG_FILE=${DATE}.jpg
 
 echo Connecting to $URL
 curl ${URL} -H "Content-Type: application/json" -d @${FILE} > $JSON_FILE
-grep 'image_path' $JSON_FILE | sed -e 's/image_path/speed/' 
+grep 'image_path' $JSON_FILE | sed -e 's/image_path/speed/'
 grep '\"image\"' $JSON_FILE | sed -e 's/.*: *//;s/[",]//g' | base64 -d > $IMG_FILE
-
